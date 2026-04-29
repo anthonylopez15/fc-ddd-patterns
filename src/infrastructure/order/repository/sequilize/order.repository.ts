168847c0da147079cc143115db1c35cfbd42643a
Anthony Lopez <tony.lopez.15@gmail.com>
2026-04-29 13:20:14 -0400

    update files

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
 export default class OrderRepository {
+
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
